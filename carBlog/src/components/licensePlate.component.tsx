9a771fb56b11b42abff058091e8d32755dfb86c4
liduoxing <liduoxing@liduoxingdeMacBook-Pro.local>
2019-12-23 17:38:58 +0800

    漏掉的

--- carBlog/src/components/licensePlate.component.tsx
+++ carBlog/src/components/licensePlate.component.tsx
@@ -10,7 +10,7 @@ export class LicensePlate extends React.Component<Props & TextProps>{
     public render() {
         const {category,carNumber,style}  = this.props
         return (
-            <View style={[{ backgroundColor: "#1E90FF", justifyContent:'center',alignItems: 'center',paddingHorizontal:5,borderRadius:3 },style]}>
+            <View style={[{ backgroundColor: "#6889ff", justifyContent:'center',alignItems: 'center',paddingHorizontal:5,borderRadius:3 },style]}>
                 <Text category={category} style={{ color: "white" }}>{carNumber}</Text>
             </View>
         )
